38182068b387fc66e617dead070c9b3559280a41
Umair Hassan <umair17@users.noreply.github.com>
2026-03-15 19:14:37 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-const CACHE = 'magyar-diak-v2';
+const CACHE = 'magyar-diak-v3';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
 ];
 
-// Install: cache everything
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE)
@@ -16,16 +18,14 @@ self.addEventListener('install', e => {
   );
 });
 
-// Activate: delete old caches
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
-// Fetch: serve from cache, fall back to network
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request))
